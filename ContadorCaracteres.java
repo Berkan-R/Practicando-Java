bea98e1aff4235c029ca93385fbaefcd07a78771
Berkan Beyhanov Raimov <berkanraimov@gmail.com>
2024-09-30 20:26:34 +0200

    Contador de caracteres

---
+++ ContadorCaracteres.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ContadorCaracteres {
+    public static void main(String[] args) {
+        
+        Scanner captador = new Scanner(System.in);
+        
+        String texto;
+
+        System.out.println("Hola soy tu contador de texto, introduce un texto: ");
+        texto = captador.nextLine();
+
+        System.out.println("La cantidad de caracteres que tienes: " + texto.replace(" ", "").length());
+    
+        captador.close();
+    }
+}
